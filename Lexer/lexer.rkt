5173e4c17288e992085788bfdc132c93b39bf5f3
David Hurst <david@ubuntu.(none)>
2013-02-01 00:30:38 -0800

    lots of cheddah

--- Lexer/lexer.rkt
+++ Lexer/lexer.rkt
@@ -6,16 +6,15 @@
 
 (define implicit-lj-level 0)
 
-(define stack (list 1))
+;;STACK FOR INDENT/DEDENT
+(define stack (list 0))
 
-(define (push x) 
+(define (push x)
   (set! stack (append (list x) stack )))
 
 (define (pop) 
   (define val (car stack))
-  (set! stack (cdr stack))
-  val
-  )
+  (set! stack (cdr stack)) val)
 
 (define (peek)
   (car stack))
@@ -38,9 +37,6 @@
                               "assert"     "else"       "import"     "pass"
                               "break"      "except"     "in"         "raise"))
 
-(define python-operators (list "+"  "-"   "*"  "**" "//" "////" "%"
-                               "<<" ">>"  "&"  "|"  "^"  "~"    "<"
-                               ">"  "<="  ">=" "==" "!="))
 
 (define-tokens jrr-tolkeins (NEWLINE INDENT DEDENT ID LIT KEYWORD PUNCT ENDMARKER))
 
@@ -118,54 +114,91 @@
 (define (dec-ilj-level)
   (set! ilj-level (- ilj-level 1)))
 
+(define ilj-comment #f)
+
+(define (set-ilj-true)
+  (set! ilj-comment #t))
+
+(define (set-ilj-false)
+  (set! ilj-comment #f))
+
 (define lj-lexer 
   (lexer
-   ["\n" (if (equal? ilj-level 0) (string-append lexeme (lj-lexer input-port)) (string-append "" (lj-lexer input-port)))]
-   [(:or "(" "[" "{") (begin (inc-ilj-level) (string-append lexeme (lj-lexer input-port)))]
-   [(:or ")" "]" "}") (begin (dec-ilj-level) (string-append lexeme (lj-lexer input-port)))]
-   [(:: "\\" "\n") (string-append "" (lj-lexer input-port))]
-   [any-char (string-append lexeme (lj-lexer input-port))]
+   ["\n"               (begin (set-ilj-false) 
+                              (if (equal? ilj-level 0)     (string-append lexeme (lj-lexer input-port)) 
+                                                           (string-append "" (lj-lexer input-port))))]
+   [#\#                (begin (set-ilj-true) (string-append lexeme (lj-lexer input-port)))]
+   [(:: #\\ (:+ "\n")) (if ilj-comment (begin (set-ilj-false) (string-append "\n" (lj-lexer input-port))) (string-append "" (lj-lexer input-port)))]
+   [any-char           (string-append lexeme                              (lj-lexer input-port))]
    [(eof) ""]
    ))
 
 
+(define luth-lexer
+  (lexer
+   [" "       (string-append "\n " (lj-lexer input-port))]
+   [#\#       (begin (set-ilj-true) (string-append "#"  (lj-lexer input-port)))]
+   [(:~ " ")  (string-append lexeme (lj-lexer input-port))]
+   [(eof)     (lj-lexer input-port)]))
+
 (define PYTHONIA-OPTIMUS-LEXER
   (lexer
+   [(:or "(" "[" "{") (begin (inc-ilj-level)   (cons `(PUNCT ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port)))]
+   [(:or ")" "]" "}") (begin (dec-ilj-level)   (cons `(PUNCT ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port)))]
    [(:: id_start (:* id_rest))   (if (member lexeme python-keywords) (cons `(KEYWORD ,(string->symbol lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))
-                                     (cons `(ID ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port)))]
-   [#\t (cons `(ERROR "Unexpected tab"))]
+                                    (cons `(ID ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port)))]
+   [#\t                             (cons `(ERROR "Unexpected tab"))]
   
-   [punct (cons `(PUNCT ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [punct                           (cons `(PUNCT ,(string-append "\"" lexeme "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
    ;;RAW STRINGS
-   [(:: rawprefix "'''")    (cons `(LIT ,(string-append "\"" (raw-singlequote-long-lex input-port) "\""))  (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: rawprefix "\"\"\"") (cons `(LIT ,(string-append "\"" (raw-doublequote-long-lex input-port) "\""))  (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: rawprefix "'")      (cons `(LIT ,(string-append "\"" (raw-singlequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: rawprefix #\")      (cons `(LIT ,(string-append "\"" (raw-doublequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: rawprefix "'''")            (cons `(LIT ,(string-append "\"" (raw-singlequote-long-lex input-port) "\""))  (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: rawprefix "\"\"\"")         (cons `(LIT ,(string-append "\"" (raw-doublequote-long-lex input-port) "\""))  (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: rawprefix "'")              (cons `(LIT ,(string-append "\"" (raw-singlequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: rawprefix #\")              (cons `(LIT ,(string-append "\"" (raw-doublequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
    ;;NORMAL
-   [(:: (:? normalprefix) "'''")    (cons `(LIT ,(string-append "\"" (singlequote-long-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: (:? normalprefix) "\"\"\"") (cons `(LIT ,(string-append "\"" (doublequote-long-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: (:? normalprefix) "'")      (cons `(LIT ,(string-append "\"" (singlequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [(:: (:? normalprefix) #\")      (cons `(LIT ,(string-append "\"" (doublequote-short-lex input-port) "\"")) (PYTHONIA-OPTIMUS-LEXER input-port))]
-
-   
-   ;;[stringliteral  (cons `(LIT ,(stringify (string-literal lexeme))) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   ;;[bytesliteral   (cons `(LIT ,(stringify (byte-literal lexeme)))   (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [floatnumber    (cons `(LIT ,(float-proc lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [decimalinteger (cons `(LIT ,(string->number lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: (:? normalprefix) "'''")    (cons `(LIT ,(string-append "\"" (singlequote-long-lex input-port) "\""))      (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: (:? normalprefix) "\"\"\"") (cons `(LIT ,(string-append "\"" (doublequote-long-lex input-port) "\""))      (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: (:? normalprefix) "'")      (cons `(LIT ,(string-append "\"" (singlequote-short-lex input-port) "\""))     (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(:: (:? normalprefix) #\")      (cons `(LIT ,(string-append "\"" (doublequote-short-lex input-port) "\""))     (PYTHONIA-OPTIMUS-LEXER input-port))]
+   ;;NUMBER LITERALS
+   [floatnumber                     (cons `(LIT ,(float-proc lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [decimalinteger                  (cons `(LIT ,(string->number lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
    [(:or bininteger hexinteger octinteger) (cons `(LIT ,(replace-numid lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
-   [imagnumber (cons `(LIT ,(replace-imag lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
-
-   [(:: (:* newlines) comment (:* space)) (cons `(NEWLINE) `, (if (tab_pre_processor (indent-length lexeme))
-                                                  (PYTHONIA-OPTIMUS-LEXER input-port)
-                                                  (tab_processor (indent-length lexeme) input-port)))]  
-   [(:: (:* (:* newlines) (:* whitespace) (:* comment)) newlines (:* space)) (cons `(NEWLINE) `, (if (tab_pre_processor (indent-length lexeme))
-                                                  (PYTHONIA-OPTIMUS-LEXER input-port)
-                                                  (tab_processor (indent-length lexeme) input-port)))]                                                
+   [imagnumber                      (cons `(LIT ,(replace-imag lexeme)) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   
+   [#\#                             (comment-lex input-port)]
+   [newlines                        (if (eq? ilj-level 0) (newline-lex input-port) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   ;;[(:: (:* newlines) comment (:* space)) (PYTHONIA-OPTIMUS-LEXER input-port)]  
+   ;;[(:: (:* (:* newlines) (:* whitespace) (:* comment)) newlines (:* space)) (cons `(NEWLINE) `, (if (tab_pre_processor (indent-length lexeme))
+   ;;                                              (PYTHONIA-OPTIMUS-LEXER input-port)
+   ;;                                              (tab_processor (indent-length lexeme) input-port)))]                                                
    [(eof) (if (> (length stack) 1) (cons (eof-dedents) `(ENDMARKER)) `((ENDMARKER)))]
 
-   [whitespace (PYTHONIA-OPTIMUS-LEXER input-port)]
-   [comment (PYTHONIA-OPTIMUS-LEXER input-port)]
+   
+   [" " (PYTHONIA-OPTIMUS-LEXER input-port)]
+   ;;[comment (PYTHONIA-OPTIMUS-LEXER input-port)]
 
+   ))
+
+(define comment-lex
+  (lexer
+   [allbutnewline         (comment-lex input-port)]
+   [#\\                   (comment-lex input-port)]
+   [(:: #\\ newlines)     (begin (display "hi")(if (eq? ilj-level 0) (newline-lex input-port) (PYTHONIA-OPTIMUS-LEXER input-port)))]
+   [newlines              (if (eq? ilj-level 0) (newline-lex input-port) (PYTHONIA-OPTIMUS-LEXER input-port))]
+   [(eof) (if (> (length stack) 1) (cons (eof-dedents) `(ENDMARKER)) `((ENDMARKER)))]
+   ))
+
+(define newline-lex
+  (lexer
+   [newlines                          (newline-lex input-port)]
+   [(:: (:* space) newlines)(newline-lex input-port)]
+   [(:: (:* space) #\#)     (comment-lex input-port)]
+   [(:* space)              (cons `(NEWLINE) (if (tab_pre_processor (string-length lexeme))
+                                                  (PYTHONIA-OPTIMUS-LEXER input-port)
+                                                  (tab_processor (string-length lexeme) input-port)))]
+   
+   [(eof)  (cons `(NEWLINE) (if (> (length stack) 1)  (eof-dedents)  `((ENDMARKER))))]
    ))
 
 ;;STRING LITERAL LEXERS
@@ -176,9 +209,6 @@
    [shortstringsingleitem (string-append lexeme (singlequote-short-lex input-port))]
    [(eof) "(ERROR \"unexpected eof"]
    ))
-
-(define (float-proc lexeme)
-  (if (equal? (string-ref lexeme (- (string-length lexeme) 1)) #\.) (string-append lexeme "0") lexeme))
 
 (define doublequote-short-lex
   (lexer
@@ -240,15 +270,13 @@
    [(eof) "(ERROR \"unexpected eof"]
    ))
 
-
+;;HELPERS
 (define (strip-newlines lst)
   (if (equal? (car lst) '(NEWLINE)) (cdr lst) lst))
 
-
-(define (decrement-lj-level)
-  (set! implicit-lj-level (- implicit-lj-level 1))
-  implicit-lj-level)
-                         
+(define (float-proc lexeme)
+  (if (equal? (string-ref lexeme (- (string-length lexeme) 1)) #\.) (string-append lexeme "0") lexeme))
+          
 (define (replace-numid lexeme)
   (string-replace lexeme "0" "#" #:all? #f))
   
@@ -256,23 +284,16 @@
   (string->number (string-append "0+" (string-replace (string-replace lexeme "j" "i") "J" "i"))))
 
 (define (eof-dedents)
-  (pop)
-  (when (> 1 (length stack)) (cons '(DEDENT) (eof-dedents)) 
-))
-  
-
-(define (stringliteral-removelinejoints lexeme)
-   (read (open-input-string lexeme)))
-  
-  ;;(regexp-replace #rx"\n" (read (open-input-string lexeme)) ""))
+ (pop)
+  (if (> (length stack) 0) (cons `(DEDENT) (eof-dedents))
+                           `((ENDMARKER))))
 
 (define (indent-length lexeme)
   (string-length (car (regexp-match #rx"\n[ ]*$" lexeme))))
 
-
 (define (run-display)
   ;;(print (lj-lexer (current-input-port)))
-  (for-each (lambda (arg) (pretty-display arg)) (strip-newlines (PYTHONIA-OPTIMUS-LEXER (open-input-string (lj-lexer (current-input-port)))))))
+  (for-each (lambda (arg) (pretty-display arg)) (strip-newlines (PYTHONIA-OPTIMUS-LEXER (open-input-string (luth-lexer (current-input-port)))))))
 
 (define (run-file filename)
   (current-input-port (open-input-file filename))
@@ -282,25 +303,8 @@
   (current-input-port (open-input-string str))
   (run-display))
 
-
 (define (string-raw str)
   (string-replace str "\\" "\\\\"))
-
-(define (byte-literal str)
-  (match (string-downcase (substring (byte-literalraw str) 0 1))
-    ["b" (substring (byte-literalraw str) 1)]
-    [_ (byte-literalraw str)]
-    ))
-
-(define (byte-literalraw str)
-  (match (string-downcase (substring str 0 2))
-        ["rb" (string-raw (substring str 2))]
-        ["br" (string-raw (substring str 2))]
-        [_ str]
-    ))
-
-;(define (stringify str)
-;  (stringme (regexp-replace #rx"\"\"\"$" (regexp-replace #rx"^\"\"\"" (regexp-replace #rx"'''$" (regexp-replace #rx"^'''" str "\"") "\"") "\"") "\"")))
 
 (run-display)
 
