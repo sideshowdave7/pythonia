68807ee8895626d39224c98171494a3cf72fd906
David Hurst <david@128-110-87-84.uconnect.utah.edu>
2013-04-11 19:18:52 -0600

    cps stuff

--- pydesugar/pycps.rkt
+++ pydesugar/pycps.rkt
@@ -1,49 +1,207 @@
 #lang racket
-; primitive-operations is a set of symbols
-(define primitive-operations
-; primitive-operation? : symbol -> boolean
-(define (primitive-operation? prim-op)
-; partition-k : ('a -> boolean) 'a list ('a list 'a list -> 'a list 'a list)
-(define (partition-k pred lst k)
-; define? : term -> boolean
-(define (define? sx)
-; atomic? : exp -> boolean
-(define (atomic? exp)
-; cps-transform-program : cps-transforms a desugared program
+
+; Input language:
+
+; <aexpr> ::= (λ (<var>*) <expr>)
+;          |  <var>
+;          |  #t | #f
+;          |  <number>
+;          |  <string>
+;          |  (void)
+;          |  call/ec | call/cc
+
+; <expr> ::= <aexpr>
+;         |  (begin <expr>*)
+;         |  (if <expr> <expr> <expr>)
+;         |  (set! <var> <expr>)
+;         |  (letrec ([<var> <aexpr>]*) <expr>)
+;         |  (<prim> <expr>*)
+;         |  (<expr> <expr>*)
+
+; <prim> = { + , - , / , * , = }
+
+
+
+; Output language:
+
+; <aexp> ::= (λ (<var>*) <cexp>)
+;         |  <var>
+;         |  #t | #f
+;         |  <number>
+;         |  <string>
+;         |  (void)
+
+; <cexp> ::= (if <aexp> <cexp> <cexp>)
+;         |  (set-then! <var> <aexp> <cexp>)
+;         |  (letrec ([<var> <aexp>]*) <cexp>)
+;         |  ((cps <prim>) <aexp>*)
+;         |  (<aexp> <aexp>*)
+
+
+(define prims
+  (apply set '( + - / *  = < > py-print not assert1 
+                  set? py-list? dict? tuple? string? integer?
+                  bitwise-not dict-remove! dict-ref tuple-ref
+                  py-list-remove! py-list-ref
+                  bitwise-and bitwise-or bitwise-xor
+                  for-dict for-tuple for-py-list for-set
+                  assert2 expt quotient modulo << >> equal? >= <= not-equal?
+                  in? not-in? eq? not-eq? py-list-set! dict-set! tuple-set!)))
+
+(define (aexpr? expr)
+  (match expr
+    [(or `(,(or 'λ 'lambda) (,_ ...) ,_)
+         (? symbol?)
+         (? number?)
+         (? string?)
+         (? boolean?)
+         '(void))
+     ; =>
+     #t]
+    
+    [else #f]))
+
+(define (prim? term)
+  (set-member? prims term))
+         
+
+(define (T-k expr k)
+  (match expr
+    [ (? aexpr?)  #;=>  (k (M expr))]
+    
+    [`(begin ,expr)
+      (T-k expr k)]
+    
+    [`(begin ,expr ,exprs ...)
+      (T-k expr (lambda (_)
+                  (T-k `(begin ,@exprs) k)))]
+    
+    [`(if ,exprc ,exprt ,exprf)
+      ; We have to reify the cont to avoid
+      ; a possible code blow-up:
+      (define $rv (gensym '$rv))
+      (define cont `(lambda (,$rv) ,(k $rv)))
+      (T-k exprc (lambda (aexp)
+           `(if ,aexp 
+                ,(T-c exprt cont)
+                ,(T-c exprf cont))))]
+    
+    [`(set! ,var ,expr)
+      (T-k expr (lambda (aexp)
+                  `(set-then! ,var ,aexp
+                              ,(k '(void)))))]
+    
+    [`(letrec ([,vs ,as] ...) ,expr)
+     `(letrec (,@(map list vs (map M as))) 
+        ,(T-k expr k))]
+    
+ 
+    
+    [`(,_ ,_ ...)
+      ; =>
+      (define $rv (gensym '$rv))
+      (define cont `(lambda (,$rv) ,(k $rv)))
+      (T-c expr cont)]))
+
+
+(define (T-c expr c)
+  (match expr
+    [ (? aexpr?)  #;=>  `(,c ,(M expr))]
+    
+    [`(begin ,expr)      (T-c expr c)]
+    
+    [`(begin ,expr ,exprs ...)
+      (T-k expr (lambda (_)
+                  (T-c `(begin ,@exprs) c)))]
+    
+    [`(if ,exprc ,exprt ,exprf)
+      ; We have to bind the cont to avoid
+      ; a possible code blow-up:
+      (define $k (gensym '$k))
+      ;`((lambda (,$k)
+          (T-k exprc (lambda (aexp)
+                        `(if ,aexp 
+                             ,(T-c exprt $k)
+                             ,(T-c exprf $k))))]
+        ;)
+        ;,c)]
+    
+    [`(set! ,var ,expr)
+      (T-k expr (lambda (aexp)
+                  `(set-then! ,var ,aexp
+                              `(,c (void)))))]
+    
+    [`(letrec ([,vs ,as] ...) ,expr)
+     `(letrec (,@(map list vs (map M as))) 
+        ,(T-c expr c))]
+    
+    [`(,(and p (? prim?)) ,es ...)
+      ; =>
+     (T*-k es (lambda ($es)
+                `((cps ,p) ,@$es ,c)))]
+    
+    [`(,f ,es ...)    
+      ; =>
+      (T-k f (lambda ($f)
+             (T*-k es (lambda ($es)
+                      `(,$f ,@$es ,c)))))]))
+
+
+(define (T*-k exprs k)
+  (cond
+    [(null? exprs)   (k '())]
+    [(pair? exprs)   (T-k (car exprs) (lambda (hd)
+                       (T*-k (cdr exprs) (lambda (tl)
+                         (k (cons hd tl))))))]))
+     
+(define (M aexpr)
+  (match aexpr
+    [`(,(or 'lambda 'λ) (,vars ...) ,body)
+      ; =>
+      (define $k (gensym '$k))
+     `(lambda (,@vars ,$k) 
+        ,(T-c body $k))]
+    
+    [(or 'call/ec 'call/cc)
+     '(lambda (f cc) (f (lambda (x _) (cc x)) cc))]
+    
+    [(or (? symbol?)     
+         (? number?)
+         (? string?)
+         (? boolean?))     
+     ; =>
+     aexpr]
+    
+    
+    [else 
+     (error "Not an aexpr!")]))
+
+
+;; Extensions
+
+(define (cps f)
+  (λ args
+    (match args
+      [`(,xs ... ,k)
+       (k (apply f xs))])))
+
+
+(define-syntax set-then!
+  (syntax-rules ()
+    [(_ var exp then)
+     (begin
+       (set! var exp)
+       then)]))
+             
+
+
 (define (cps-transform-program program)
-; cps-transform-def : cps transforms a variable definition
-(define (cps-transform-def def)
-; app : construct a call form
-; ex: (app 'f 1 2 'x) => '(f 1 2 x)
-(define (app f . args)
-; cont->kont converts a syntactic continuation
-;            into a meta-continuation
-; ex: (cont->kont '(λ (x) (f x))
-;       =>
-;     (λ (rv) `((λ (x) (f x)) ,rv)
-(define (cont->kont q)
-; kont->cont converts a meta-continuation
-;            into a syntactic continuation
-; ex: (kont->cont k) 
-;       =>
-;    `(λ ($rv123) ,(k $rv123))
-(define (kont->cont k)
-; let-cont fakes a let form by immediately
-;          applying a lambda form
-(define (let-cont v exp body)
-; let-name binds an expression to a value if
-;          duplicatin it code bloat code
-(define (let-name exp k)
-; cps-atom converts an atomic value to an atomic cps value
-(define (cps-atom exp)
-; cps-transform-k converts an expression to cps and calls
-;                 k with an atom holding its return value;
-;                 k is a meta-continuation
-(define (cps-transform-k exp k)
-; cps-transform-q converts an expression to cps and
-;                 inserts a call to q with its return value
-;                 q is a syntactic continuation
-(define (cps-transform-q exp q)
-; cps-transform-k* : exp list (exp list -> answer) -> answer
-(define (cps-transform-k* exps k)
-(pretty-write (cps-transform-program (read)))
+  (match program
+    
+    [`(program . ,stmts) `(program ,@(map cps-transform-program stmts)) ]
+    [`(define ,v ,e) `(define ,v ,e)]
+    [stmt  (T-c stmt '$halt)]
+  ))
+  
+
+(pretty-write (cps-transform-program (read (open-input-file "test.py"))))
